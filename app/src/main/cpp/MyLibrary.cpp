526ca34dcee2f9812032b9a15fe13e55ec43ff44
Fahad Habib <109348151+fahaddhabib@users.noreply.github.com>
2024-07-10 23:17:55 +0500

    Update MyLibrary.cpp

--- app/src/main/cpp/MyLibrary.cpp
+++ app/src/main/cpp/MyLibrary.cpp
@@ -1,14 +1,18 @@
 #include <jni.h>
 #include <iostream>
+#include <string>
+
+std::string storedMessage;
 
 extern "C" JNIEXPORT void JNICALL
 Java_com_example_testtask_MyLibrary_passMessage(JNIEnv* env, jobject obj, jstring message) {
     const char* nativeMessage = env->GetStringUTFChars(message, nullptr);
+    storedMessage = nativeMessage;
     std::cout << "Received message: " << nativeMessage << std::endl;
     env->ReleaseStringUTFChars(message, nativeMessage);
 }
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_testtask_MyLibrary_getMessage(JNIEnv* env, jobject obj) {
-    return env->NewStringUTF("Message from C++");
+    return env->NewStringUTF(storedMessage.c_str());
 }
